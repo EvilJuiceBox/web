dd4aaabb69399d681f7afdf7e8eb636b2c85bd1c
Kira <tokirachan@gmail.com>
2023-10-13 20:53:03 -0400

    added front page work for about me

--- js/index.js
+++ js/index.js
@@ -4,9 +4,11 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("header").style.padding = "30px 10px";
-    document.getElementById("header").style.height = "15em";
+    document.getElementById("header").style.height = "5em";
+    document.getElementById("main").style.padding = "padding: 6em 0 0 0";
   } else {
-    document.getElementById("header").style.padding = "80px 10px";
-    document.getElementById("header").style.height = "25em";
+    document.getElementById("header").style.padding = "0 0 0 0";
+    document.getElementById("header").style.height = "15em";
+    document.getElementById("main").style.padding = "padding: 11em 0 0 0";
   }
 }
