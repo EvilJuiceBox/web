dadf17b90af0d7ae92ea578acc3174c14d73a2f6
Kira <tokirachan@gmail.com>
2023-10-15 04:19:02 -0400

    fixed small bugs with navbar scalability

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,12 +3,12 @@ function toggleHamburger() {
     let header = document.getElementById("header");
     let navbar = document.getElementById("navbar");
     console.log("toggling")
-    if (x.style.display === "block") {
+    if (x.style.display === "inline-block") {
       x.style.display = "none";
       header.style.height = "20vh";
       navbar.style.height = "5vh";
     } else {
-      x.style.display = "block";
+      x.style.display = "inline-block";
       header.style.height = "40vh";
       navbar.style.height = "20vh";
     }
