6be549b3000d431e5549ab199a336951c80c7923
Kira <tokirachan@gmail.com>
2023-10-16 17:34:15 -0400

    fixed grammar on paragraphs, added ability of random quote to fade in and out

--- js/random.js
+++ js/random.js
@@ -4,14 +4,19 @@
 
 
 function rotateRandomText(text_object, random_text) {
-    text_object.innerText = "Random: " + random_text[Math.floor(Math.random()*random_text.length)];
+    document.getElementById("random").setAttribute("class", "random_fade");
+    
+    setTimeout(() => {
+        text_object.innerText = "Random: " + random_text[Math.floor(Math.random()*random_text.length)];
+        document.getElementById("random").setAttribute("class", "random_show");
+    }, 1000);
     // setTimeout("rotateRandomText()", 1000);
 }
 
 $(function() { // shorthand for `$(document).ready(function() {
     const text_object = document.getElementById("random_quote");
     // list of random facts
-    const random_text = ["I grow my hair out to donate it annually for charity!", "Go Green!", "Pasta is my favourite food.", "I apprenticed under Michigan State University's head chef Kurt Kwiatkowski during my undergrad.", "My favourite colour is amethyst", "Standing on the shoulders of giants"];
-    setInterval(rotateRandomText, 10000, text_object, random_text);
+    const random_text = ["I grow my hair out to donate it annually for charity!", "Go Green!", "Pasta is my favourite food.", "I apprenticed under Michigan State University's head chef Kurt Kwiatkowski during my undergrad.", "My favourite colour is amethyst", "Standing on the shoulders of giants", "Ed Sheeran is my favourite artist.", "I enjoy building Gundam Models as a hobby."];
+    setInterval(rotateRandomText, 8000, text_object, random_text);
 });
 
