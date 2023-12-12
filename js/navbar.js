fc74563736f7b9bf81b2d07d1cafc1b502847b2c
Kira <tokirachan@gmail.com>
2023-12-12 16:58:19 -0500

    added dropdown menu to redirect to page sections. Added js code in navbar.js to not scroll past the navbar length (covers content), redid graphics for cse335

--- js/navbar.js
+++ js/navbar.js
@@ -3,3 +3,24 @@
 $(function() { // shorthand for `$(document).ready(function() {
     $("#header").load("./navbar.html");
 });
+
+$(function () {
+    // Scroll to function 
+    function scrollTo(ele) {
+        $("html, body").animate({
+            scrollTop: $(ele).offset().top - $("#header").outerHeight()
+        });
+    }
+
+    // Detect location hash
+    if (window.location.hash) {
+        scrollTo(window.location.hash);
+    }
+
+    // Detect click event
+    $("header a[href^='#']").click(function (e) {
+        var target = $(this).attr("href");
+        scrollTo(target);
+        e.preventDefault();
+    });
+});
