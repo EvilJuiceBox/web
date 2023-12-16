fead3352b085b4b6da5d9b6ed02316cd0bba89e4
Kira <tokirachan@gmail.com>
2023-12-16 00:56:43 -0500

    moved anchors around

--- js/navbar.js
+++ js/navbar.js
@@ -4,23 +4,23 @@ $(function() { // shorthand for `$(document).ready(function() {
     $("#header").load("./navbar.html");
 });
 
-$(function () {
-    // Scroll to function 
-    function scrollTo(ele) {
-        $("html, body").animate({
-            scrollTop: $(ele).offset().top - $("#header").height()
-        });
-    }
+// $(function () {
+//     // Scroll to function 
+//     function scrollTo(ele) {
+//         $("html, body").animate({
+//             scrollTop: $(ele).offset().top - $("#header").height()
+//         });
+//     }
 
-    // Detect location hash
-    if (window.location.hash) {
-        scrollTo(window.location.hash);
-    }
+//     // Detect location hash
+//     if (window.location.hash) {
+//         scrollTo(window.location.hash);
+//     }
 
-    // Detect click event
-    $("header a[href^='#']").click(function (e) {
-        var target = $(this).attr("href");
-        scrollTo(target);
-        e.preventDefault();
-    });
-});
+//     // Detect click event
+//     $("header a[href^='#']").click(function (e) {
+//         var target = $(this).attr("href");
+//         scrollTo(target);
+//         e.preventDefault();
+//     });
+// });
